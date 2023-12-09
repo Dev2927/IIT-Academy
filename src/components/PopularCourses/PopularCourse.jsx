83807658c1498e11a41ceab830324cf79ffda092
Dev anand <devanand290702@gmail.com>
2023-12-09 23:51:34 +0530

    Updated

--- src/components/PopularCourses/PopularCourse.jsx
+++ src/components/PopularCourses/PopularCourse.jsx
@@ -1,22 +1,29 @@
 import React, { useState } from "react";
 import "./Tabs.css";
 import { courseData } from "../PopularCourses/CourseData";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 import previous from "../../assets/previous.png";
 import next from "../../assets/next.png";
 
 function PopularCourse() {
   const [tab, setTab] = useState(1);
-  const [currentPage, setCurrentPage] = useState(1);
 
   function activeTab(tabNumber) {
     setTab(tabNumber);
-    setCurrentPage(1);
   }
 
   const engineer = courseData.filter((course) => course.item === "engineering");
-  const medicalCourse = courseData.filter((course) => course.item === "medical");
-  const foudations = courseData.filter((course) => course.item === "foundation");
-  const distanceLearning = courseData.filter((course) => course.item === "distance");
+  const medicalCourse = courseData.filter(
+    (course) => course.item === "medical"
+  );
+  const foudations = courseData.filter(
+    (course) => course.item === "foundation"
+  );
+  const distanceLearning = courseData.filter(
+    (course) => course.item === "distance"
+  );
   const boards = courseData.filter((course) => course.item === "board");
 
   const data =
@@ -32,20 +39,40 @@ function PopularCourse() {
       ? boards
       : null;
 
-  const pageSize = 3;
-  const totalPageCount = Math.ceil(data.length / pageSize);
-
-  const handlePrevious = () => {
-    setCurrentPage((prevPage) => Math.max(prevPage - 1, 1));
+  const settings = {
+    dots: true,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    previous: <CustomPrevArrow />,
+    next: <CustomNextArrow />,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          initialSlide: 2,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
   };
-
-  const handleNext = () => {
-    setCurrentPage((prevPage) => Math.min(prevPage + 1, totalPageCount));
-  };
-
-  const startIndex = (currentPage - 1) * pageSize;
-  const endIndex = startIndex + pageSize;
-  const visibleData = data && data.slice(startIndex, endIndex);
 
   return (
     <div className="course-container">
@@ -55,63 +82,105 @@ function PopularCourse() {
         of their academic growth by bringing out their latent potential
       </p>
       <div className="course">
-        <div
-          onClick={() => activeTab(1)}
-          className="course-btn course-btns radius"
-        >
-          Engineering
-        </div>
-        <div onClick={() => activeTab(2)} className="course-btn course-btns">
-          Medical
-        </div>
-        <div onClick={() => activeTab(3)} className="course-btn course-btns">
-          Foundation
-        </div>
-        <div onClick={() => activeTab(4)} className="course-btn course-btns">
-          Distance Learning
-        </div>
-        <div
-          onClick={() => activeTab(5)}
-          className="course-btn course-btns radius1"
-        >
-          Boards
+        <div className="course">
+          <div
+            onClick={() => activeTab(1)}
+            className={`course-btn course-btns radius ${
+              tab === 1 ? "active-tab" : ""
+            }`}
+          >
+            Engineering
+          </div>
+          <div
+            onClick={() => activeTab(2)}
+            className={`course-btn course-btns ${
+              tab === 2 ? "active-tab" : ""
+            }`}
+          >
+            Medical
+          </div>
+          <div
+            onClick={() => activeTab(3)}
+            className={`course-btn course-btns ${
+              tab === 3 ? "active-tab" : ""
+            }`}
+          >
+            Foundation
+          </div>
+          <div
+            onClick={() => activeTab(4)}
+            className={`course-btn course-btns ${
+              tab === 4 ? "active-tab" : ""
+            }`}
+          >
+            Distance Learning
+          </div>
+          <div
+            onClick={() => activeTab(5)}
+            className={`course-btn course-btns radius1 ${
+              tab === 5 ? "active-tab" : ""
+            }`}
+          >
+            Boards
+          </div>
         </div>
       </div>
+
       <div className="detail-box">
-        {visibleData &&
-          visibleData.map((value, index) => {
-            return (
-              <div key={index} className="card-detail">
-                <img
-                  src={value.img}
-                  alt={value.description}
-                  width="300px"
-                  height="300px"
-                />
-                <p className="details-desc">{value.description}</p>
-                <div className="details-btns">
-                  <button className="details-btn">Details</button>
+        <Slider {...settings}>
+          {data &&
+            data.map((value, index) => {
+              return (
+                <div key={index} className="card-detail">
+                  <img
+                    src={value.img}
+                    alt={value.description}
+                    width="400px"
+                    height="300px"
+                  />
+                  <div className="details-btns">
+                    <p className="details-desc">{value.description}</p>
+                    <button className="details-btn">Details</button>
+                  </div>
                 </div>
-              </div>
-            );
-          })}
+              );
+            })}
+        </Slider>
       </div>
-      {data && data.length > pageSize && (
-        <div className="slider-buttons">
-          {currentPage > 1 && (
-            <button onClick={handlePrevious} className="img-prev">
-              <img src={previous} alt="Prev Img.." className="img-previous" />
-            </button>
-          )}
-          {currentPage < totalPageCount && (
-            <button onClick={handleNext} className="img-nex">
-              <img src={next} alt="Next Img" className="img-next" />
-            </button>
-          )}
-        </div>
-      )}
     </div>
   );
 }
+
+const CustomPrevArrow = (props) => {
+  const { className, style, onClick } = props;
+  return (
+    <div
+      className={className}
+      style={{
+        ...style,
+        width: "50px",
+        height: "50px",
+        backgroundImage: `url(${previous})`, // Fix: Change prevArrow to previous
+      }}
+      onClick={onClick}
+    />
+  );
+};
+
+const CustomNextArrow = (props) => {
+  const { className, style, onClick } = props;
+  return (
+    <div
+      className={className}
+      style={{
+        ...style,
+        width: "50px",
+        height: "50px",
+        backgroundImage: `url(${next})`, // Fix: Change nextArrow to next
+      }}
+      onClick={onClick}
+    />
+  );
+};
 
 export default PopularCourse;
